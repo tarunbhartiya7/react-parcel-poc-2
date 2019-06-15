86ee18f5db6b88f7b6fae607b2e93f03d934cb73
Tarun Bhartiya <tarunbhartiya@Taruns-MacBook-Pro.local>
2019-06-15 23:08:05 +0530

    error boundary and react context

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 import { Router, Link } from "@reach/router";
 import Details from "./Details";
+import ThemeContext from "./ThemeContext";
 
 const App = () => {
+  const theme = useState("peru");
   return (
     <React.StrictMode>
-      <div id="this-is-how-you-add-attribute">
-        <header>
-          <Link to="/">Adopt Me!</Link>
-        </header>
-        <Router>
-          <SearchParams path="/" />
-          <Details path="/details/:id" />
-        </Router>
-      </div>
+      <ThemeContext.Provider value={theme}>
+        <div id="this-is-how-you-add-attribute">
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
     </React.StrictMode>
   );
 };
